a8f8ff58acb94451f700fadcb2dbd9b6edc2a1cc
tytyp100 <127055540+tytyp100@users.noreply.github.com>
2025-05-24 00:47:05 -0400

    Add files via upload

---
+++ app/components/Header.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useLocation } from "react-router";
+import { cn } from "~/lib/utils";
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+const Header = ({ title, description }: Props) => {
+  const location = useLocation();
+  return (
+    <header className="header">
+      <article>
+        <h1
+          className={cn(
+            "text-dark-100",
+            location.pathname === "/"
+              ? "text-2xl md:text-4xl font-bold"
+              : "test-xl md:text-2xl font-semibold"
+          )}
+        >
+          {title}
+        </h1>
+        <p
+          className={cn(
+            "text-gray-100 font-normal",
+            location.pathname === "/"
+              ? "text-base md:text-lg"
+              : "test-sm md:text-lg"
+          )}
+        >
+          {description}
+        </p>
+      </article>
+      <div>
+        <button></button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
