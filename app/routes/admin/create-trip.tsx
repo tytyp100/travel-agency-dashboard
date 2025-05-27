2dff94e9d50790ca57a38ed8f064554f02fb8506
tytyp100 <ianhdtran@gmail.com>
2025-05-26 20:12:18 -0400

    Add trip creation API endpoint and update create trip form handling

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -12,6 +12,7 @@ import {
 import { world_map } from "~/constants/world_map";
 import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 import { account } from "~/appwrite/client";
+import { useNavigate } from "react-router";
 
 export const loader = async () => {
   const response = await fetch("https://restcountries.com/v3.1/all");
@@ -26,6 +27,7 @@ export const loader = async () => {
 
 const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
   const countries = loaderData as Country[];
+  const navigate = useNavigate();
   const [formData, setFormData] = useState<TripFormData>({
     country: countries[0]?.name || "",
     travelStyle: "",
@@ -61,10 +63,26 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
       return;
     }
     try {
-      console.log("user", user);
-      console.log("formData", formData);
-    } catch (e) {
-      console.error("Error generating trip", e);
+      const response = await fetch("/api/create-trip", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          country: formData.country,
+          numberOfDays: formData.duration,
+          travelStyle: formData.travelStyle,
+          interests: formData.interest,
+          budget: formData.budget,
+          groupType: formData.groupType,
+          userId: user.$id,
+        }),
+      });
+
+      const result: CreateTripResponse = await response.json();
+
+      if (result?.id) navigate(`/trips/${result.id}`);
+      else console.error("Failed to generate itinerary");
+    } catch (error) {
+      console.error("Error generating itinerary:", error);
     } finally {
       setLoading(false);
     }
