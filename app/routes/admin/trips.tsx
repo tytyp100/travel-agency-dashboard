e9b8ee1f238d95200fc88180943a4e57305a5b5c
tytyp100 <ianhdtran@gmail.com>
2025-05-27 18:27:42 -0400

    Finished the All trips page

--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -1,7 +1,40 @@
 import React from "react";
 import { Header } from "../../components";
+import { getAllTrips, getTripById } from "~/appwrite/trips";
+import { parseTripData } from "~/lib/utils";
+import type { Route } from "./+types/trips";
+import { useSearchParams, type LoaderFunctionArgs } from "react-router";
+import { TripCard } from "~/components";
+import { useState } from "react";
+import { PagerComponent } from "@syncfusion/ej2-react-grids";
 
-const trips = () => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const limit = 8;
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get("page") || "1", 10);
+  const offset = (page - 1) * limit;
+  const { allTrips, total } = await getAllTrips(limit, offset);
+  return {
+    trips: allTrips.map(({ $id, tripDetail, imageUrls }) => ({
+      id: $id,
+      ...parseTripData(tripDetail),
+      imageUrls: imageUrls ?? [],
+    })),
+    total,
+  };
+};
+
+const Trips = ({ loaderData }: Route.ComponentProps) => {
+  const trips = loaderData?.trips as Trip[] | [];
+
+  const [searchParam] = useSearchParams();
+  const initialPage = Number(searchParam.get("page") || 1);
+
+  const [currentPage, setCurrentPage] = useState(initialPage);
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    window.location.search = `?page=${page}`;
+  };
   return (
     <main className="all-users wrapper">
       <Header
@@ -10,8 +43,33 @@ const trips = () => {
         ctaText="Create Trip"
         ctaUrl="/trips/create"
       />
+      <section>
+        <h1 className="p-24-semibold text-dark-100 mb-4">
+          Manage Created Trips
+        </h1>
+        <div className="trip-grid mb-4">
+          {trips.map((trip) => (
+            <TripCard
+              id={trip.id}
+              key={trip.id}
+              name={trip.name}
+              imageUrl={trip.imageUrls[0]}
+              location={trip.itinerary?.[0]?.location ?? ""}
+              tags={[trip.interests, trip.travelStyle]}
+              price={trip.estimatedPrice}
+            />
+          ))}
+        </div>
+        <PagerComponent
+          totalRecordsCount={loaderData?.total}
+          pageSize={8}
+          currentPage={currentPage}
+          click={(args) => handlePageChange(args.currentPage)}
+          cssClass="!mb-4"
+        />
+      </section>
     </main>
   );
 };
 
-export default trips;
+export default Trips;
