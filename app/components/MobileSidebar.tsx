efa07911978f293daddcfcea5f8fa360e12ba047
tytyp100 <ianhdtran@gmail.com>
2025-05-26 20:16:38 -0400

    Fixed menu logo on mobile view

--- app/components/MobileSidebar.tsx
+++ app/components/MobileSidebar.tsx
@@ -25,7 +25,7 @@ const MobileSidebar = () => {
         </Link>
 
         <button onClick={() => toggleSidebar()}>
-          <img src="assets/icons/menu.svg" alt="menu" className="size-7" />
+          <img src="/assets/icons/menu.svg" alt="menu" className="size-7" />
         </button>
       </header>
       <SidebarComponent
