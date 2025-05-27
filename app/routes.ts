cc0bb2937c48df54e433ad1af64d1cbd8af58ea1
tytyp100 <ianhdtran@gmail.com>
2025-05-27 17:30:04 -0400

    Add trip detail page with Appwrite integration

--- app/routes.ts
+++ app/routes.ts
@@ -7,6 +7,7 @@ export default [
         route('dashboard','routes/admin/dashboard.tsx'),
         route('all-users','routes/admin/all-users.tsx'),
         route('trips','routes/admin/trips.tsx'),
-        route('trips/create','routes/admin/create-trip.tsx')
+        route('trips/create','routes/admin/create-trip.tsx'),
+        route('trips/:tripdId','routes/admin/trip-detail.tsx')
     ]),
 ] satisfies RouteConfig;
